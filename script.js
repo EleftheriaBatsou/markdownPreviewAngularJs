06af654fc9942d08143d509f313b682a97f55cc1
Eleftheria <eleftheriabatsou@gmail.com>
2018-02-02 00:30:42 +0200

    project for freecodecamp

---
+++ script.js
@@ -0,0 +1,16 @@
+angular.module('markdownApp', ['ngSanitize'])
+
+.controller('markdownController', function($scope) {
+    $scope.input = 'Enter your [Markdown][1] here.' +
+        '\n' +
+        '\n- *first*' +
+        '\n- **second**' +
+        '\n- third' +
+        '\n' +
+        '\n[1]: http://daringfireball.net/projects/markdown/syntax';
+})
+
+.filter('markdown', function() {
+    var converter = new Showdown.converter();
+    return converter.makeHtml;
+});
